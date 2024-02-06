0690afac62bb73d7d5cfbeedcf658180fc594958
me me <106704354+thisiscoding1234@users.noreply.github.com>
2024-02-06 14:00:05 +0000

    d

--- src/main.py
+++ src/main.py
@@ -2,15 +2,28 @@ import inputs as inp
 import housmans as hman
 
 hman.intal()
-
-print("welcome")
+name = input("what is your name: ")
+print(f"welcome, {name}!")
 ro = hman.get_room(0)
 print(hman.decode(ro))
+rm = 0
 
 while True:
     try:
+        prev = rm
         rm = inp.intinput("room num: ", 1, 1, 10)
+        if rm not in hman.decode(ro, 1):
+            print("Nice try, but you can't go there!")
+            continue
         ro = hman.get_room(rm)
-        print(hman.decode(ro))
+        print(hman.decode(ro, 0, prev))
+        if rm == 0:
+            des = input("are you sure you want to leave? (y/n): ")
+            if des == "y":
+                break
+            else:
+                pass
     except IndexError:
         print("no!")
+        
+print(f"goodbye, {name}!")
