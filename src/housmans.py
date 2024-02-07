91fc9194f2c314c515d5e33e55c7fdab82901ed9
me me <106704354+thisiscoding1234@users.noreply.github.com>
2024-02-07 16:30:42 +0000

    v

--- src/housmans.py
+++ src/housmans.py
@@ -3,6 +3,17 @@ import ast
 
 
 class room:
+    """
+    A class representing a room in the game.
+
+    Attributes:
+        name (str): The name of the room.
+        access (list): A list of access points to the room.
+        light (int): The light level in the room.
+        locked (int): The lock status of the room.
+        desc (str): The description of the room.
+    """
+
     def __init__(
         self, name: str, access: list, light: int, locked: int, desc: str
     ) -> None:
@@ -13,25 +24,51 @@ class room:
         self.desc = desc
 
     def __repr__(self) -> str:
-        return f'room("{self.name}", {self.access}, {self.light}, {self.locked}, "{self.desc}") | '
-
+        return f'room("
+    {self.name}", {self.access}, {self.light}, {self.locked}, "{self.desc}") | '
 
 def intal():
+    """
+    Initializes the house by creating rooms and writing them to a file.
+
+    Returns:
+        str: A message indicating that the house has been initialized.
+    """
     with open("house.txt", "w") as file:
         x = [
-            room("door", [1], 1, 0, "you are at the door"),
+            room(
+                "door", [1], 1, 0, "you are at the door"
+                ),
             room(
                 "hall 0", [2, 3, 4, 5, 6, 0], 1, 0, "it is the hallway on the bottom floor"
             ),
-            room("room 0-1", [1], 0, 0, "this is the first room"),
-            room("stair 0-1", [1, 7], 0, 1, "it is the hallway on the bottom floor"),
-            room("room 0-2", [1], r.randint(0, 1), r.randint(0, 1), "this is the 2 room"),
-            room("room 0-3", [1], r.randint(0, 1), r.randint(0, 1), "this is the 3 room"),
-            room("room 0-4", [1], r.randint(0, 1), r.randint(0, 1), "this is the 4 room"),
-            room("hall 1", [8, 3], 1, 0, "this is the hallway on the first floor"),
-            room("room 0-1", [7, 9], 0, 1, "this is the first room"),
-            room("stairs", [8, 10], 1, 1, "this is the stairs"),
-            room("End?", [9], 0, 1, "this is the End?"),
+            room(
+                "room 0-1", [1], 0, 0, "this is the first room"
+                ),
+            room(
+                "stair 0-1", [1, 7], 0, 1, "it is the hallway on the bottom floor"
+                ),
+            room(
+                "room 0-2", [1], r.randint(0, 1), r.randint(0, 1), "this is the 2 room"
+                ),
+            room(
+                "room 0-3", [1], r.randint(0, 1), r.randint(0, 1), "this is the 3 room"
+                ),
+            room(
+                "room 0-4", [1], r.randint(0, 1), r.randint(0, 1), "this is the 4 room"
+                ),
+            room(
+                "hall 1", [8, 3], 1, 0, "this is the hallway on the first floor"
+                ),
+            room(
+                "room 0-1", [7, 9], 0, 1, "this is the first room"
+                ),
+            room(
+                "stairs", [8, 10], 1, 1, "this is the stairs"
+                ),
+            room(
+                "End?", [9], 0, 1, "this is the End?"
+                ),
             "",
         ]
 
@@ -43,15 +80,35 @@ def intal():
 
 
 def get_room(num):
-    with  open("house.txt", "r") as file:
+    """
+    Retrieve the room at the specified index from the 'house.txt' file.
+
+    Args:
+        num (int): The index of the room to retrieve.
+
+    Returns:
+        str: The room at the specified index.
+
+    """
+    with open("house.txt", "r") as file:
         x = file.read()
         t = x.split(" | ,")
         return t[num]
 
 
-def decode(l, get_acc=0, prev=0):
-    print(l)
-    rf = eval(str(l))
+def decode(list):
+    """
+    Decode the given input and return a formatted string describing the current location and its properties.
+
+    Args:
+        l (object): The input object to decode.
+        prev (int, optional): The previous value. Defaults to 0.
+
+    Returns:
+        str: A formatted string describing the current location and its properties.
+    """
+    print(list)
+    rf = eval(str(list))
     x = rf.desc
     i = rf.light
     c = rf.locked
@@ -66,17 +123,78 @@ def decode(l, get_acc=0, prev=0):
         pass
 
     s = f"""
-    
     you are in: {loc}, which is: {x}
     you can go to: {rf.access}
     """
-
-    if get_acc == 1:
-        s = rf.access
     return s
 
 
-def get_lock(l):
+def get_lock(list):
+    """
+    Returns the 'locked' attribute of the given object.
+
+    Args:
+        l: The object to retrieve the 'locked' attribute from.
+
+    Returns:
+        The value of the 'locked' attribute.
+
+    """
+    rf = eval(str(list))
+    return rf.locked
+
+def get_acc(l):
+    """
+    Returns the 'access' attribute of the given object.
+
+    Args:
+        l: The object to retrieve the 'access' attribute from.
+
+    Returns:
+        The value of the 'access' attribute.
+
+    """
     rf = eval(str(l))
-    c = rf.locked
-    return c
+    return rf.access
+
+def get_light(list):
+    """
+    Returns the 'light' attribute of the given object.
+
+    Args:
+        l: The object to retrieve the 'light' attribute from.
+
+    Returns:
+        The value of the 'light' attribute.
+
+    """
+    rf = eval(str(list))
+    return rf.light
+
+def get_desc(list):
+    """
+    Returns the 'desc' attribute of the given object.
+
+    Args:
+        l: The object to retrieve the 'desc' attribute from.
+
+    Returns:
+        The value of the 'desc' attribute.
+
+    """
+    rf = eval(str(list))
+    return rf.desc
+
+def get_name(list):
+    """
+    Returns the 'name' attribute of the given object.
+
+    Args:
+        l: The object to retrieve the 'name' attribute from.
+
+    Returns:
+        The value of the 'name' attribute.
+
+    """
+    rf = eval(str(list))
+    return rf.name
--- src/main.py
+++ src/main.py
@@ -12,7 +12,7 @@ while True:
     try:
         prev = rm
         rm = inp.intinput("room num: ", 1, 1, 10)
-        if rm not in hman.decode(ro, 1):
+        if rm not in hman.get_acc(ro):
             print("Nice try, but you can't go there!")
             continue
         if hman.get_lock(hman.get_room(rm)) == 1:
@@ -21,7 +21,7 @@ while True:
                 "Nice try, but you can't go there, as the room is locked. Try and find a key to unlock it!"
             )
         ro = hman.get_room(rm)
-        print(hman.decode(ro, 0, prev))
+        print(hman.decode(ro))
         if rm == 0:
             des = input("are you sure you want to leave? (y/n): ")
             if des == "y":
